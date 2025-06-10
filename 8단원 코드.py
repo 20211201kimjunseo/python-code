4bb42b935d63161877da6d04b923813c064f7c10
20211201kimjunseo <junseo4580@naver.com>
2025-06-11 01:00:16 +0900

    Update 8단원 코드.py

--- 8단원 코드.py
+++ 8단원 코드.py
@@ -70,31 +70,39 @@ for student in students:
     print(student_to_string(student))
 
 # 469p 클래스 내부에 함수(메소드) 선언하기
-class student:
+class Student:
     def __init__(self, name, korean, math, english, science):
         self.name = name
         self.korean = korean
         self.math = math
         self.english = english
         self.science = science
+
     def get_sum(self):
-        return self.korean + self.math +\
-        self.english + self.science
+        return self.korean + self.math + self.english + self.science
+
     def get_average(self):
         return self.get_sum() / 4
+
     def to_string(self):
-        return "{}\t{}\t{}".format(
-            self.name,\
-            self.get_sum(),\
-            self.get_average())
-    student = [
-    create_student("윤인성", 87, 98, 88, 95),
-    create_student("연하진", 82, 98, 96, 98),
-    create_student("구지연", 76, 96, 94, 90),
-    create_student("나선주", 98, 92, 96, 92),
-    create_student("윤아린", 95, 98, 98, 98),
-    create_student("윤명월", 64, 88, 92, 92),
+        return "{}\t{}\t{:.2f}".format(
+            self.name,
+            self.get_sum(),
+            self.get_average()
+        )
+
+students = [
+    Student("윤인성", 87, 98, 88, 95),
+    Student("연하진", 82, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92),
 ]
+
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+    print(student.to_string())
 print("이름", "총점", "평균", sep="\t")
 for student in students:
     print(student.to_string())
